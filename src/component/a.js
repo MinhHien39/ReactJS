3d66c33861e657d8e7072c9145b48cba4705ac3c
MinhHien39 <tranminhhien06072001@gmail.com>
2025-01-21 22:04:49 +0700

    [MERGE] test feature v1

--- src/component/a.js
+++ src/component/a.js
@@ -8,7 +8,7 @@ const TestComponent = ({ title, onClick }) => {
     <div style={styles.container}>
       <h1 style={styles.title}>{title || "Default Title"}</h1>
       <button style={styles.button} onClick={onClick}>
-        123123Click Me12123123
+        123123Click Me12123123ASDASD
       </button>
     </div>
   );
