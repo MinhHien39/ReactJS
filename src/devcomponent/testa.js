4725f212264d57bb3decd35b859cd5bcf3a1c91a
MinhHien39 <tranminhhien06072001@gmail.com>
2025-01-21 20:52:38 +0700

    [MERGE] test-feature2

--- src/devcomponent/testa.js
+++ src/devcomponent/testa.js
@@ -8,7 +8,7 @@ const TestComponent = ({ title, onClick }) => {
     <div style={styles.container}>
       <h1 style={styles.title}>{title || "Default Title"}</h1>
       <button style={styles.button} onClick={onClick}>
-        Click Me
+        Click Me1
       </button>
     </div>
   );
