246697d65315e62c5de8e8a64993f8319031165c
MinhHien39 <tranminhhien06072001@gmail.com>
2025-01-22 00:38:14 +0700

    [MERGE] test lai lan 16

--- src/component/b.js
+++ src/component/b.js
@@ -9,7 +9,7 @@ const TestComponent = ({ title, onClick }) => {
       <h1 style={styles.title}>{title || "Default Title"}</h1>
       <button style={styles.button} onClick={onClick}>
 
-        BBCCDDEEFF2233445566778899001122334455
+        BBCCDDEEFF223344556677889900112233445566
 
       </button>
     </div>
