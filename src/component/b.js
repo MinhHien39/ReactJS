e00b5df090c63d55317af4da397b550b2e1b763c
MinhHien39 <tranminhhien06072001@gmail.com>
2025-01-22 19:08:55 +0700

    [MERGE] test-1

--- src/component/b.js
+++ src/component/b.js
@@ -9,7 +9,7 @@ const TestComponent = ({ title, onClick }) => {
       <h1 style={styles.title}>{title || "Default Title"}</h1>
       <button style={styles.button} onClick={onClick}>
 
-        BASCSSSS
+        BASCSSSS22
 
       </button>
     </div>
