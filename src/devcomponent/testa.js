9bf2d1e5faabdc182d1baaaacd3d4924a6ad2004
MinhHien39 <tranminhhien06072001@gmail.com>
2025-01-22 19:37:00 +0700

    [MERGE] test-6

--- src/devcomponent/testa.js
+++ src/devcomponent/testa.js
@@ -9,7 +9,7 @@ const TestComponent = ({ title, onClick }) => {
       <h1 style={styles.title}>{title || "Default Title"}</h1>
       <button style={styles.button} onClick={onClick}>
 
-        CCDDEEFFGG33445566
+        CCDDEEFFGG3344556677
 
       </button>
     </div>
